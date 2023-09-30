67dac129ae4ef3284d79d7992eb2a5ce2c7705cd
Andreas Aronsson <BOLL@7708.net>
2023-09-30 16:03:22 +0200

    Add database for used icons

---
+++ src/DB.ts
@@ -0,0 +1,68 @@
+import sqlite3 from 'sqlite3'
+import {Database, open} from 'sqlite'
+import fs from 'fs/promises'
+
+/**
+ * Class that handles database operations.
+ */
+export default class DB {
+    constructor() {
+        const dir = './db'
+        fs.access(dir)
+            .then(() => console.log('DB directory exists'))
+            .catch(() => {
+                fs.mkdir('./db')
+                    .then(() => console.log('DB directory created'))
+                    .catch((err) => console.error('Unable to create DB directory', err))
+            })
+    }
+
+    private _db: Database<sqlite3.Database> | undefined = undefined
+
+    private async getDb(): Promise<Database<sqlite3.Database> | undefined> {
+        if (!this._db) {
+            this._db = await open({
+                filename: './db/server_bot.db',
+                driver: sqlite3.Database
+            })
+            this._db.on('trace', (data) => {
+                console.log('SQL Trace:', data)
+            })
+        }
+        return this._db
+    }
+
+    private async ensureServerIconTable() {
+        const db = await this.getDb()
+        await db.exec('CREATE TABLE IF NOT EXISTS server_icons (id INTEGER PRIMARY KEY, file TEXT, date TEXT)')
+    }
+
+    /**
+     * Will register an icon as used.
+     * @param file File name of the icon.
+     * @param date Date the icon was used.
+     */
+    async registerServerIcon(file: string, date: string): Promise<boolean> {
+        const db = await this.getDb()
+        if (db) {
+            await this.ensureServerIconTable()
+            const stmt = await db.prepare('INSERT INTO server_icons (file, date) VALUES (?,?)')
+            const result = await stmt.run(file, date)
+            if (result.lastID) return true
+        }
+        return false
+    }
+
+    /**
+     * Will return all icons that have previously been used.
+     */
+    async getAllServerIcons(): Promise<string[]> {
+        const db = await this.getDb()
+        if (db) {
+            await this.ensureServerIconTable()
+            const result = await db.all('SELECT file FROM server_icons')
+            return result.map(row => row.file)
+        }
+        return []
+    }
+}
--- src/ServerBot.ts
+++ src/ServerBot.ts
@@ -2,9 +2,14 @@
 // https://discordjs.guide/creating-your-bot/main-file.html#running-your-application
 import {AttachmentBuilder, Client, Events, ForumChannel, GatewayIntentBits, ThreadAutoArchiveDuration} from 'discord.js'
 import fs from 'fs/promises'
+import DB from './DB.js'
 
+/**
+ * The Discord server bot.
+ */
 export default class ServerBot {
     private _config: IConfig
+    private _db = new DB()
 
     async start() {
         const client: Client = new Client({intents: [GatewayIntentBits.Guilds]})
@@ -29,17 +34,33 @@ export default class ServerBot {
     }
 
     private async setServerIcon(client: Client) {
-        // Load image file
-        const path = './images/icons'
+        // Load image files
+        const path = './images/icon_test' // TODO: Switch to live folder after test
         const files = await fs.readdir(path)
-        // TODO: We should load which files have already been used and remove them from the pool.
+
+        console.log('files', files)
+
+        // Load which files have already been used and remove them from the pool
+        const existingIcons = await this._db.getAllServerIcons()
+        for (const existingIcon of existingIcons) {
+            const index = files.indexOf(existingIcon)
+            if (index > -1) files.splice(index, 1)
+        }
+
+        // Pick a random file
         const file = files[Math.floor(Math.random() * files.length)]
-        const filePath = `${path}/${file}`
+        const isNewFile = !!file
+        const filePath = isNewFile
+            ? `${path}/${file}`
+            : `./images/icon.png`
+
+        console.log('filePath', filePath)
 
         // Set server icon
         const guild = client.guilds.cache.get(this._config.serverId)
         const iconResult = await guild.setIcon(filePath)
-        if (!iconResult) console.error('Failed to set server icon.') // TODO: Pretty sure I have to check a value inside the response.
+        if (!iconResult) return console.error('Failed to set server icon.') // TODO: Pretty sure I have to check a value inside the response.
+        if(!isNewFile) return console.log('No new file so reset to default and will not post image or save in DB.')
 
         // Post server icon to forum channel
         let date = new Date()
@@ -58,9 +79,15 @@ export default class ServerBot {
             })
             if (!channelResponse) console.error('Failed to create thread.') // TODO: Not sure if this is the right way to check for success.
         }
+
+        // Register in DB
+        await this._db.registerServerIcon(file, dateStr)
     }
 }
 
+/**
+ * The configuration stored on file as config.json or config.local.json.
+ */
 interface IConfig {
     clientId: string
     clientSecret: string
